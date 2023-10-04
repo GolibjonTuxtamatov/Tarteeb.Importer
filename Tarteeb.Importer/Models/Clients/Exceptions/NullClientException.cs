9d89c185ec10c7b18971c91dfc08ad3d61cef8ed
GolibjonTuxtamatov <tuxtamatovgolibjon@gamil.com>
2023-10-04 20:10:04 +0500

    POC: Add Client -> WIP

---
+++ Tarteeb.Importer/Models/Clients/Exceptions/NullClientException.cs
@@ -0,0 +1,16 @@
+﻿//=================================
+// CoopyRight (c) Tarteeb LLC
+// Powering True Leadership
+//=================================
+
+using Xeptions;
+
+namespace Tarteeb.Importer.Models.Clients.Exceptions
+{
+    internal class NullClientException : Xeption
+    {
+        public NullClientException()
+            :base("Client is null")
+        { }
+    }
+}
--- Tarteeb.Importer/Program.cs
+++ Tarteeb.Importer/Program.cs
@@ -5,11 +5,13 @@
 
 using Tarteeb.Importer.Brokers.Storages;
 using Tarteeb.Importer.Models.Clients;
+using Tarteeb.Importer.Models.Clients.Exceptions;
+using Tarteeb.Importer.Services.Clients;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello World");
+        Console.WriteLine("Hello World!");   
     }
 }
