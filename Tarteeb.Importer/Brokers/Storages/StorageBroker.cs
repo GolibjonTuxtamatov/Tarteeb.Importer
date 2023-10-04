272ea9d645dc59c953bd2372539341edfd0948ad
GolibjonTuxtamatov <tuxtamatovgolibjon@gamil.com>
2023-10-04 17:53:55 +0500

    DATA: Model & Migrate Client

---
+++ Tarteeb.Importer/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,41 @@
+﻿//=================================
+// CoopyRight (c) Tarteeb LLC
+// Powering True Leadership
+//=================================
+
+
+using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+using Tarteeb.Importer.Models.Clients;
+
+namespace Tarteeb.Importer.Brokers.Storages
+{
+    internal class StorageBroker : EFxceptionsContext
+    {
+        public DbSet<Client> Clients { get; set; }
+
+        public StorageBroker() =>
+            this.Database.EnsureCreated();
+
+        public IQueryable<Client> SelectAllClients() =>
+            this.Clients.AsQueryable();
+
+        public async Task<Client> InsertClientAsync(Client client)
+        {
+            await this.Clients.AddAsync(client);
+            await this.SaveChangesAsync();
+
+            return client;
+        }
+
+        public async Task<Client> SelectClientById(Guid clientId) =>
+            await this.Clients.FindAsync(clientId);
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Data Source = ../../../Tarteeb.db";
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            optionsBuilder.UseSqlite(connectionString);
+        }
+    }
+}
