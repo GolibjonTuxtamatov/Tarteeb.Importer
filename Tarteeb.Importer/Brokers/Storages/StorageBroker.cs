0f790efc23a9546ca553fcce236813086f3f3cce
GolibjonTuxtamatov <tuxtamatovgolibjon@gamil.com>
2023-10-04 18:48:32 +0500

    BROKERS: Insert Client

--- Tarteeb.Importer/Brokers/Storages/StorageBroker.cs
+++ Tarteeb.Importer/Brokers/Storages/StorageBroker.cs
@@ -12,6 +12,16 @@ namespace Tarteeb.Importer.Brokers.Storages
 {
     internal class StorageBroker : EFxceptionsContext
     {
+        public DbSet<Client> Clients { get; set; }
+
+        public async Task<Client> InsertClientAsync(Client client)
+        {
+            await this.Clients.AddAsync(client);
+            await this.SaveChangesAsync();
+
+            return client;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string connectionString = "Data Source = ../../../Tarteeb.db";
