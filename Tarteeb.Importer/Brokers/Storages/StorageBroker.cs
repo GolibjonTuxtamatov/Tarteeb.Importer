bda7b7103c621b801c638ec59edc238074834218
GolibjonTuxtamatov <tuxtamatovgolibjon@gamil.com>
2023-10-04 18:32:30 +0500

    CODE RUB: Remove Methods

--- Tarteeb.Importer/Brokers/Storages/StorageBroker.cs
+++ Tarteeb.Importer/Brokers/Storages/StorageBroker.cs
@@ -12,25 +12,6 @@ namespace Tarteeb.Importer.Brokers.Storages
 {
     internal class StorageBroker : EFxceptionsContext
     {
-        public DbSet<Client> Clients { get; set; }
-
-        public StorageBroker() =>
-            this.Database.EnsureCreated();
-
-        public IQueryable<Client> SelectAllClients() =>
-            this.Clients.AsQueryable();
-
-        public async Task<Client> InsertClientAsync(Client client)
-        {
-            await this.Clients.AddAsync(client);
-            await this.SaveChangesAsync();
-
-            return client;
-        }
-
-        public async Task<Client> SelectClientById(Guid clientId) =>
-            await this.Clients.FindAsync(clientId);
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string connectionString = "Data Source = ../../../Tarteeb.db";
